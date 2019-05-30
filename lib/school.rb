7900e08baddd2a2274ff60bb89ce90fc95536a6e
Melissa Williams <melissa@brandmeetscopy.com>
2019-05-30 01:26:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,21 +5,6 @@ class School
 
 ROSTER = {}
 
-def initialize(name)
-  @name = name
-end
-
-def roster=(roster)
-  @roster = roster
-end
-
-
-
-
-def name=(name)
-  @name = name
-end
-
 def add_student(name, grade)
 end
 
